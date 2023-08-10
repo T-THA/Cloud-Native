6158d5adb4f58a3b53842bf5addabb011b3f9c9d
TTHA <626661840@qq.com>
2023-08-11 03:41:34 +0800

    ADD a rest interface

--- src/main/java/com/example/cloudnative/Controller/DemoController.java
+++ src/main/java/com/example/cloudnative/Controller/DemoController.java
@@ -1,7 +1,33 @@
 package com.example.cloudnative.Controller;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+
+    @GetMapping("/api/text")
+    @ResponseStatus(HttpStatus.OK)
+    public String getText() {
+        return "Hello World!";
+    }
+
+    @GetMapping( "/api/json")
+    @ResponseStatus(HttpStatus.OK)
+    public String getJson() {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("name", "TTHA");
+            json.put("age", 20);
+            json.put("msg", "hello");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        String ret = json.toString();
+        return ret;
+    }
 }
